597ce2dd36d642d89b465b014e611c4c0f39c06e
Jon Harmon <jonthegeek@gmail.com>
2023-08-27 15:35:37 -0500

    Add S7-in-package helpers. (#22)
    * Add S7-in-package helpers.
    
    * Avoid the onAttach bit.
    
    That generates an R CMD check warning.

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
+
+.onLoad <- function(...) {
+  S7::methods_register()
+}
