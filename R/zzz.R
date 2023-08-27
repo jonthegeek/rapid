4547bf4bd6068911bfcddb93d0d7cd8908023f4a
Jon Harmon <jonthegeek@gmail.com>
2023-08-27 15:27:44 -0500

    Avoid the onAttach bit.
    That generates an R CMD check warning.

--- R/zzz.R
+++ R/zzz.R
@@ -2,11 +2,6 @@
 #' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 NULL
 
-.onAttach <- function(libname, pkgname) {
-  if (getRversion() < "4.3.0")
-    require(S7)
-}
-
 .onLoad <- function(...) {
   S7::methods_register()
 }
