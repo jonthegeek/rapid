0c4002aa14ea93c90a2e2e81fc735699268ee2ef
Jon Harmon <jonthegeek@gmail.com>
2023-09-08 16:18:23 -0500

    shared-params isn't used

--- R/00-shared-params.R
+++
@@ -1,8 +0,0 @@
-#' Parameters used in multiple functions
-#'
-#' Reused parameter definitions are gathered here for easier editing.
-#'
-#' @param ... Catch-all for unimplemented properties.
-#' @name .shared-parameters
-#' @keywords internal
-NULL
