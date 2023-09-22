74fb55211692d289eb42ead03e2c9c74b68e3c12
Jon Harmon <jonthegeek@gmail.com>
2023-09-22 09:16:59 -0500

    Refactor.

--- R/components-security_scheme-oauth2-token_flows.R
+++ R/components-security_scheme-oauth2-token_flows.R
@@ -64,59 +64,46 @@ S7::method(length, oauth2_token_flows) <- function(x) {
   length(x@token_url)
 }
 
+# TODO: fix help in all the as_ functions to point out that we snakecase-ize the
+# names.
+
 #' Coerce lists to OAuth2 token flows
 #'
-#' `as_api_key_security_scheme()` turns an existing object into a
-#' `api_key_security_scheme`. This is in contrast with
-#' [api_key_security_scheme()], which builds a `api_key_security_scheme` from
-#' individual properties.
+#' `as_oauth2_token_flows()` turns an existing object into a
+#' `oauth2_token_flows`. This is in contrast with [oauth2_token_flows()], which
+#' builds a `oauth2_token_flows` from individual properties.
 #'
 #' @inheritParams rlang::args_dots_empty
 #' @param x The object to coerce. Must be empty or be a list of named lists,
-#'   each with names "description", "name", and/or "in". Additional names are
-#'   ignored.
+#'   each with names "refresh_url", "scopes", and/or "token_url". Additional
+#'   names are ignored.
 #'
-#' @return A `api_key_security_scheme` as returned by
-#'   [api_key_security_scheme()].
+#' @return A `oauth2_token_flows` as returned by [oauth2_token_flows()].
 #' @export
-as_api_key_security_scheme <- S7::new_generic(
-  "as_api_key_security_scheme",
+as_oauth2_token_flows <- S7::new_generic(
+  "as_oauth2_token_flows",
   dispatch_args = "x"
 )
 
-S7::method(as_api_key_security_scheme, api_key_security_scheme) <- function(x) {
+S7::method(as_oauth2_token_flows, oauth2_token_flows) <- function(x) {
   x
 }
 
-S7::method(as_api_key_security_scheme, class_list) <- function(x) {
-  call <- rlang::caller_env()
-  x <- purrr::map(
-    x,
-    function(x) {
-      .validate_for_as_class(
-        x,
-        api_key_security_scheme,
-        extra_names = "in",
-        x_arg = "x[[i]]",
-        call = call
-      )
-    }
-  )
+S7::method(as_oauth2_token_flows, class_list) <- function(x) {
+  x <- .validate_for_as_class(x, oauth2_token_flows)
 
-  nameless <- unname(x)
-  api_key_security_scheme(
-    name = names(x),
-    description = .extract_along_chr(nameless, "description"),
-    parameter_name = purrr::map_chr(nameless, "name"),
-    location = purrr::map_chr(nameless, "in")
+  oauth2_token_flows(
+    token_url = x$token_url,
+    refresh_url = x$refresh_url,
+    scopes = scopes_list(as_scopes(x$scopes))
   )
 }
 
-S7::method(as_api_key_security_scheme, class_missing | NULL) <- function(x) {
-  api_key_security_scheme()
+S7::method(as_oauth2_token_flows, class_missing | NULL) <- function(x) {
+  oauth2_token_flows()
 }
 
-S7::method(as_api_key_security_scheme, class_any) <- function(x) {
+S7::method(as_oauth2_token_flows, class_any) <- function(x) {
   cli::cli_abort(
     "Can't coerce {.arg x} {.cls {class(x)}} to {.cls api_key_security_scheme}."
   )
