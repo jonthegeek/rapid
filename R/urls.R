dc2e9350fc753661b3a4ca546e4de2db0eea68a5
Jon Harmon <jonthegeek@gmail.com>
2024-03-26 13:30:38 -0500

    Better read.

--- R/urls.R
+++ R/urls.R
@@ -4,7 +4,12 @@
 
 .url_fetch <- function(x) {
   rlang::try_fetch(
-    jsonlite::read_json(x),
+    jsonlite::read_json(
+      x,
+      simplifyVector = TRUE,
+      simplifyDataFrame = FALSE,
+      simplifyMatrix = FALSE
+    ),
     error = function(e) {
       yaml::read_yaml(url(x))
     }
